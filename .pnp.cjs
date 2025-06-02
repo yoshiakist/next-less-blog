11157248658a97faa62e7cd7b2890266aec517a5
Yoshiaki Sato <yoshiaki.st@gmail.com>
2025-06-02 15:31:28 +0900

    chore: biome の導入

--- .pnp.cjs
+++ .pnp.cjs
@@ -28,6 +28,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         [null, {\
           "packageLocation": "./",\
           "packageDependencies": [\
+            ["@biomejs/biome", "npm:1.9.4"],\
             ["@types/node", "npm:20.14.11"],\
             ["@types/react", "npm:18.3.3"],\
             ["@types/react-dom", "npm:18.3.0"],\
@@ -64,6 +65,95 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [\
             ["@babel/runtime", "npm:7.24.8"],\
             ["regenerator-runtime", "npm:0.14.1"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["@biomejs/biome", [\
+        ["npm:1.9.4", {\
+          "packageLocation": "./.yarn/unplugged/@biomejs-biome-npm-1.9.4-9e22cce864/node_modules/@biomejs/biome/",\
+          "packageDependencies": [\
+            ["@biomejs/biome", "npm:1.9.4"],\
+            ["@biomejs/cli-darwin-arm64", "npm:1.9.4"],\
+            ["@biomejs/cli-darwin-x64", "npm:1.9.4"],\
+            ["@biomejs/cli-linux-arm64", "npm:1.9.4"],\
+            ["@biomejs/cli-linux-arm64-musl", "npm:1.9.4"],\
+            ["@biomejs/cli-linux-x64", "npm:1.9.4"],\
+            ["@biomejs/cli-linux-x64-musl", "npm:1.9.4"],\
+            ["@biomejs/cli-win32-arm64", "npm:1.9.4"],\
+            ["@biomejs/cli-win32-x64", "npm:1.9.4"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["@biomejs/cli-darwin-arm64", [\
+        ["npm:1.9.4", {\
+          "packageLocation": "./.yarn/unplugged/@biomejs-cli-darwin-arm64-npm-1.9.4-b529d719c2/node_modules/@biomejs/cli-darwin-arm64/",\
+          "packageDependencies": [\
+            ["@biomejs/cli-darwin-arm64", "npm:1.9.4"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["@biomejs/cli-darwin-x64", [\
+        ["npm:1.9.4", {\
+          "packageLocation": "./.yarn/unplugged/@biomejs-cli-darwin-x64-npm-1.9.4-cf32790440/node_modules/@biomejs/cli-darwin-x64/",\
+          "packageDependencies": [\
+            ["@biomejs/cli-darwin-x64", "npm:1.9.4"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["@biomejs/cli-linux-arm64", [\
+        ["npm:1.9.4", {\
+          "packageLocation": "./.yarn/unplugged/@biomejs-cli-linux-arm64-npm-1.9.4-70c93a9413/node_modules/@biomejs/cli-linux-arm64/",\
+          "packageDependencies": [\
+            ["@biomejs/cli-linux-arm64", "npm:1.9.4"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["@biomejs/cli-linux-arm64-musl", [\
+        ["npm:1.9.4", {\
+          "packageLocation": "./.yarn/unplugged/@biomejs-cli-linux-arm64-musl-npm-1.9.4-8f7e7ca6fe/node_modules/@biomejs/cli-linux-arm64-musl/",\
+          "packageDependencies": [\
+            ["@biomejs/cli-linux-arm64-musl", "npm:1.9.4"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["@biomejs/cli-linux-x64", [\
+        ["npm:1.9.4", {\
+          "packageLocation": "./.yarn/unplugged/@biomejs-cli-linux-x64-npm-1.9.4-2d8259aa1b/node_modules/@biomejs/cli-linux-x64/",\
+          "packageDependencies": [\
+            ["@biomejs/cli-linux-x64", "npm:1.9.4"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["@biomejs/cli-linux-x64-musl", [\
+        ["npm:1.9.4", {\
+          "packageLocation": "./.yarn/unplugged/@biomejs-cli-linux-x64-musl-npm-1.9.4-5f09c730ea/node_modules/@biomejs/cli-linux-x64-musl/",\
+          "packageDependencies": [\
+            ["@biomejs/cli-linux-x64-musl", "npm:1.9.4"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["@biomejs/cli-win32-arm64", [\
+        ["npm:1.9.4", {\
+          "packageLocation": "./.yarn/unplugged/@biomejs-cli-win32-arm64-npm-1.9.4-c04e8f8a59/node_modules/@biomejs/cli-win32-arm64/",\
+          "packageDependencies": [\
+            ["@biomejs/cli-win32-arm64", "npm:1.9.4"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["@biomejs/cli-win32-x64", [\
+        ["npm:1.9.4", {\
+          "packageLocation": "./.yarn/unplugged/@biomejs-cli-win32-x64-npm-1.9.4-76ccca98e5/node_modules/@biomejs/cli-win32-x64/",\
+          "packageDependencies": [\
+            ["@biomejs/cli-win32-x64", "npm:1.9.4"]\
           ],\
           "linkType": "HARD"\
         }]\
@@ -3841,6 +3931,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./",\
           "packageDependencies": [\
             ["next-less-blog", "workspace:."],\
+            ["@biomejs/biome", "npm:1.9.4"],\
             ["@types/node", "npm:20.14.11"],\
             ["@types/react", "npm:18.3.3"],\
             ["@types/react-dom", "npm:18.3.0"],\
